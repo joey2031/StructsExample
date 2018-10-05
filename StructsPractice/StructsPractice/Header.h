4b4e8ea409474b3b77cedd69cf536011b8499a1e
joey2031 <36198917+joey2031@users.noreply.github.com>
2018-10-05 11:46:06 -0400

    Add files via upload

---
+++ StructsPractice/StructsPractice/Header.h
@@ -0,0 +1,6 @@
+struct human
+{
+	char firstName[10];
+	char lastName[10];
+	int age;
+};
---
+++ StructsPractice/StructsPractice/mainc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "Header.h"
+
+
+int main(void) {
+	struct human bob = {"billy", "bob", 18};
+
+	printf("This is the first name: %s\n", bob.firstName);
+	printf("This is the last name: %s\n", bob.lastName);
+	printf("This is the age: %d\n", bob.age);
+
+
+	printf("Enter a number to change the age variable in the struct: ");
+	scanf("%d", &bob.age);
+	printf("The new value in the age varable of the struct human is: %d", bob.age);
+
+
+
+
+
+}
